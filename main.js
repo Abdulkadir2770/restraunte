910e485cf322ff2f9e17016d588ae2de703bc380
Amadi Y <absnacks17@gmail.com>
2022-09-01 12:32:19 -0500

    n/a

--- main.js
+++ main.js
@@ -10,13 +10,33 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/home.js":
-/*!*********************!*\
-  !*** ./src/home.js ***!
-  \*********************/
+/***/ "./src/home-page/home.js":
+/*!*******************************!*\
+  !*** ./src/home-page/home.js ***!
+  \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"displayHomePage\": () => (/* binding */ displayHomePage)\n/* harmony export */ });\nconst displayHomePage = (() => {\r\n   // the content <div>\r\n   const content = document.querySelector(\"#content\");\r\n   \r\n   const heroSection = document.createElement(\"div\");\r\n   heroSection.classList.add(\".hero-section\");\r\n   heroSection.innerHTML = `\r\n      <div id=\"info\">\r\n         <h3>Welcome to Binti's Takeout</h3>\r\n         <br>\r\n         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n      </div>\r\n      <div>\r\n         <img src=\"../dist/assets/img/pasta.jpg\" alt=\"pasta image\"/>\r\n      </div>\r\n   `;\r\n\r\n   content.appendChild(heroSection);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/home.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"displayHomePage\": () => (/* binding */ displayHomePage)\n/* harmony export */ });\nconst displayHomePage = (() => {\r\n   // the content <div>\r\n   const content = document.querySelector(\"#content\");\r\n   \r\n   const heroSection = document.createElement(\"div\");\r\n   heroSection.classList.add(\"hero-section\");\r\n   heroSection.innerHTML = `\r\n      <div id=\"info\">\r\n         <h3>Welcome to Binti's Takeout</h3>\r\n         <br>\r\n         <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n      </div>\r\n      <div>\r\n         <img src=\"../dist/assets/img/pasta.jpg\" alt=\"pasta image\"/>\r\n      </div>\r\n   `;\r\n\r\n   content.appendChild(heroSection);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/home-page/home.js?");
+
+/***/ }),
+
+/***/ "./src/home-page/onPageLoad.js":
+/*!*************************************!*\
+  !*** ./src/home-page/onPageLoad.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"onPageLoad\": () => (/* binding */ onPageLoad)\n/* harmony export */ });\nconst onPageLoad = (() => {\r\n    const mainContainer = document.querySelector(\"#content\");\r\n\r\n    // main header\r\n    const header = document.createElement(\"header\");\r\n    header.innerHTML = `<h1>Binti's Takeout</h1> <img src='../dist/assets/img/logo.jpg' alt=\"logo img\" id=\"logo\"/>`;\r\n\r\n    mainContainer.appendChild(header);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/home-page/onPageLoad.js?");
+
+/***/ }),
+
+/***/ "./src/home-page/secondaryNav.js":
+/*!***************************************!*\
+  !*** ./src/home-page/secondaryNav.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"secondaryNav\": () => (/* binding */ secondaryNav)\n/* harmony export */ });\nconst secondaryNav = (() => {\r\n    // secondary header\r\n    const mainContainer = document.querySelector(\"#content\");\r\n    const header = document.querySelector(\"header\");\r\n\r\n    const secondaryHeader = document.createElement(\"div\");\r\n    secondaryHeader.classList.add(\"secondary-nav\");\r\n    secondaryHeader.innerHTML = `\r\n        <div class=\"sn-item\">Menu</div>\r\n        <div class=\"sn-item\">About</div>\r\n        <div class=\"sn-item\">Contact</div>\r\n    `;\r\n\r\n\r\n    mainContainer.appendChild(secondaryHeader);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/home-page/secondaryNav.js?");
 
 /***/ }),
 
@@ -26,27 +46,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _onPageLoad__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./onPageLoad */ \"./src/onPageLoad.js\");\n/* harmony import */ var _secondaryNav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./secondaryNav */ \"./src/secondaryNav.js\");\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/onPageLoad.js":
-/*!***************************!*\
-  !*** ./src/onPageLoad.js ***!
-  \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"onPageLoad\": () => (/* binding */ onPageLoad)\n/* harmony export */ });\nconst onPageLoad = (() => {\r\n    const mainContainer = document.querySelector(\"#content\");\r\n\r\n    // main header\r\n    const header = document.createElement(\"header\");\r\n    header.innerHTML = `<h1>Binti's Takeout</h1> <img src='../dist/assets/img/logo.jpg' alt=\"logo img\" id=\"logo\"/>`;\r\n\r\n    mainContainer.appendChild(header);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/onPageLoad.js?");
-
-/***/ }),
-
-/***/ "./src/secondaryNav.js":
-/*!*****************************!*\
-  !*** ./src/secondaryNav.js ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"secondaryNav\": () => (/* binding */ secondaryNav)\n/* harmony export */ });\nconst secondaryNav = (() => {\r\n    // secondary header\r\n    const mainContainer = document.querySelector(\"#content\");\r\n    const header = document.querySelector(\"header\");\r\n\r\n    const secondaryHeader = document.createElement(\"div\");\r\n    secondaryHeader.classList.add(\"secondary-nav\");\r\n    secondaryHeader.innerHTML = `\r\n        <div class=\"sn-item\">Menu</div>\r\n        <div class=\"sn-item\">About</div>\r\n        <div class=\"sn-item\">Contact</div>\r\n    `;\r\n\r\n\r\n    mainContainer.appendChild(secondaryHeader);\r\n})();\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/secondaryNav.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home_page_onPageLoad__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home-page/onPageLoad */ \"./src/home-page/onPageLoad.js\");\n/* harmony import */ var _home_page_secondaryNav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home-page/secondaryNav */ \"./src/home-page/secondaryNav.js\");\n/* harmony import */ var _home_page_home__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./home-page/home */ \"./src/home-page/home.js\");\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://restraunte/./src/index.js?");
 
 /***/ })
 
