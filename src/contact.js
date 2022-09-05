3bf95a491699e90f9717b285b05d68d7ba7ddb47
Amadi Y <absnacks17@gmail.com>
2022-09-05 11:11:29 -0500

    completed contact page

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,24 @@ const contactPage = () => {
     const container = document.querySelector(".hero-section");
 
     container.innerHTML = `
-        <div>Contact us</h1>
+        <div id="contact-info">
+            <h1>Contact us:</h1>
+            <div>
+                <h4>Phone</h4>
+                <ul>
+                    <li>879-867-5767</li>
+                </ul>    
+            </div>
+            <div>
+                <h4>Addrees</h4>
+                <ul>
+                    <li>12345 My Personal Address Rd, Rock Sheet TX 79890.</li>
+                </ul>
+            </div>
+        </div>
+        <div id="map">
+            <img src="../dist/assets/img/googlemap.gif" />
+        </div>
     `;
 
     console.log("Contact page displayed");
